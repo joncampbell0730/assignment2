03705aaefaf15306b875a802e36b40639100d3bd
campbj1 <jonathan.campbell@dillards.com>
2021-12-30 13:52:59 -0600

    new years

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { PasswordComponent } from './password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
